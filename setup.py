b352ab6e36868b7ca588b5c75032c1b64f22c10f
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2013-05-16 09:19:52 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='django-cadolib',
+    version='0.1.0',
+    description='Simple set of tiny usefull bits',
+    author='Frank Wawrzak (CadoSolutions)',
+    author_email='frank.wawrzak@cadosolutions.com',
+    url='https://github.com/fsw/django-cadolib',
+    download_url='git://github.com/fsw/django-cadolib.git',
+    packages=['cadolib'],
+)
