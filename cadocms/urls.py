a85dce1fc8b4a1efe8882a9f4422c10ba184a7c3
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2013-09-21 12:50:15 -0500

    put extrafields behind https

--- cadocms/urls.py
+++ cadocms/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     url(r'^admin/rebuildindex/$', 'cadocms.views.admin_rebuildindex'),
     url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^extrafields/(?P<model>[A-Za-z0-9\-\.\_]*)/(?P<provider_id>\d+)$', 'cadocms.views.extrafields'),
+    url(r'^accounts/extrafields/(?P<model>[A-Za-z0-9\-\.\_]*)/(?P<provider_id>\d+)$', 'cadocms.views.extrafields'),
     url(r'^api/children/(?P<model>[A-Za-z0-9\-\.\_]*)/(?P<parent_id>\d+)/$', 'cadocms.views.api_tree_children'),
     url(r'^api/path/(?P<model>[A-Za-z0-9\-\.\_]*)/(?P<item_id>\d+)/$', 'cadocms.views.api_tree_path'),
     url(r'^api/fullpath/(?P<model>[A-Za-z0-9\-\.\_]*)/(?P<item_id>\d+)/$', 'cadocms.views.api_tree_fullpath'),
