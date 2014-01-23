a43dcab1b04b5468cbe06bb8b0655ca29b6d7aef
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2014-01-23 08:29:39 -0600

    fix

--- cadocms/widgets.py
+++ cadocms/widgets.py
@@ -65,7 +65,7 @@ class HTMLFieldWidgetTrivial(forms.Textarea):
     class Media:
         js = (
               settings.STATIC_URL + 'tinymce/tinymce.min.js',
-              settings.STATIC_URL + 'js/tinymce_setup_trivial.js?v=4',
+              settings.STATIC_URL + 'js/tinymce_setup_trivial.js?v=5',
         )
         
     def render(self, name, value, attrs=None):
