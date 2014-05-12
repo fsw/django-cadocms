669efc7d4f8c60496ea4bb7688a768b21425d828
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2014-05-12 18:47:56 +1000

    EDL-43

--- cadocms/email.py
+++ cadocms/email.py
@@ -1,5 +1,5 @@
 from django.template import loader, Context, RequestContext
-import html2text
+from html2text import HTML2Text
 from django.core.mail import send_mail, EmailMultiAlternatives
 from django.conf import settings
 
@@ -7,8 +7,10 @@ def StandardEmail(template, subject, from_email, to_email, variables, request = 
         
     html_content = loader.render_to_string(template, variables, None if request is None else RequestContext(request))
     #https://jugger0.atlassian.net/browse/EDL-43
-    text_content = html2text.html2text(html_content).replace("](", "]\n(");
-    
+    #text_content = html2text.html2text(html_content).replace("](", "]\n(");
+    h = HTML2Text(baseurl='');
+    h.inline_links = False;
+    text_content = h.handle(html_content)
       
     msg = EmailMultiAlternatives(subject, text_content, from_email, to_email, headers = headers)
     msg.attach_alternative(html_content, "text/html")
